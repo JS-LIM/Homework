928ed0371200a89076f52aea258ad23c99e87be9
JS-LIM <neyo0311@naver.com>
2016-10-09 00:35:57 +0900

    Add files via upload

---
+++ Circle.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "GeometricObject.h"
+
+void drawCircle(const int& x_center, const int& y_center, const int& r, const int& red, const int& green, const int& blue);
+class Circle : public GeometricObject
+{
+
+public:
+	int start_x, start_y;
+	int r;
+	Circle(const int& _start_x, const int& _start_y, const int& _r)
+	{
+		initialize(_start_x, _start_y, _r);
+	}
+
+	void initialize(const int& _start_x, const int& _start_y, const int& _r);
+
+	void draw();
+};
---
+++ Triangle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "GeometricObject.h"
+void drawTriangle(const int& i0, const int& j0, const float& red, const float& green, const float& blue);
+
+class Triangle : public GeometricObject 
+{
+public:
+	int start_x, start_y;
+	int end_x, end_y;
+	Triangle(const int& _start_x, const int& _start_y, const int& _end_x, const int& _end_y) 
+	{
+		initialize(_start_x, _start_y, _end_x, _end_y);
+	}
+	void initialize(const int& _start_x, const int& _start_y, const int& _end_x, const int& _end_y);
+
+	void draw();
+};
